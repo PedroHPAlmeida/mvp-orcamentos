b6381fb368d4bab79685c00d16c6294252b410f7
Pedro Henrique P. Almeida <pedro.6571almeida@gmail.com>
2025-04-04 18:49:54 -0300

    feat: first version of the new budget screen

--- app/src/main/java/br/com/budgets/MainActivity.kt
+++ app/src/main/java/br/com/budgets/MainActivity.kt
@@ -1,47 +1,26 @@
 package br.com.budgets
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import br.com.budgets.ui.theme.BudgetsTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BudgetsTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    NewBudgetScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BudgetsTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/br/com/budgets/NewBudgetScreen.kt
@@ -0,0 +1,220 @@
+package br.com.budgets
+
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun NewBudgetScreen() {
+    val datePickerState = rememberDatePickerState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.customer_plural),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedButton(
+            modifier = Modifier.fillMaxWidth(), onClick = {}) {
+            Icon(
+                imageVector = Icons.Default.AddCircle,
+                contentDescription = null,
+                modifier = Modifier.size(20.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = stringResource(R.string.add_button))
+        }
+
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 16.dp), color = Color.Black, thickness = 2.dp
+        )
+
+        Text(
+            text = stringResource(R.string.products_and_services),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedButton(
+            modifier = Modifier.fillMaxWidth(), onClick = {}) {
+            Icon(
+                imageVector = Icons.Default.AddCircle,
+                contentDescription = null,
+                modifier = Modifier.size(20.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = stringResource(R.string.add_button))
+        }
+
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 16.dp), color = Color.Black, thickness = 2.dp
+        )
+
+        Text(
+            text = stringResource(R.string.place_and_date),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = "",
+            onValueChange = {},
+            label = { Text(stringResource(R.string.city_hint)) })
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        DatePickerFieldToModal()
+
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 16.dp), color = Color.Black, thickness = 2.dp
+        )
+
+        Text(
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            text = stringResource(R.string.budget_details),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Row {
+            Text(text = stringResource(R.string.items_quantity))
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = "0")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row {
+            Text(text = stringResource(R.string.total))
+            Spacer(modifier = Modifier.weight(1f))
+            Text(text = "R$ 0,00")
+        }
+
+        // To align the button to the end of the screen
+        Spacer(modifier = Modifier.weight(1f))
+
+        OutlinedButton(
+            modifier = Modifier.fillMaxWidth(), onClick = {}) {
+            Text(text = stringResource(R.string.generate_budget))
+        }
+    }
+}
+
+@Composable
+fun DatePickerFieldToModal(modifier: Modifier = Modifier) {
+    var selectedDate by remember { mutableStateOf<Long?>(null) }
+    var showModal by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = selectedDate?.let { convertMillisToDate(it) } ?: "",
+        onValueChange = { },
+        label = { Text(stringResource(R.string.select_date)) },
+        placeholder = { Text(stringResource(R.string.br_date_format)) },
+        trailingIcon = {
+            Icon(Icons.Default.DateRange, contentDescription = "Select date")
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .pointerInput(selectedDate) {
+                awaitEachGesture {
+                    awaitFirstDown(pass = PointerEventPass.Initial)
+                    val upEvent = waitForUpOrCancellation(pass = PointerEventPass.Initial)
+                    if (upEvent != null) {
+                        showModal = true
+                    }
+                }
+            })
+
+    if (showModal) {
+        DatePickerModal(onDateSelected = { selectedDate = it }, onDismiss = { showModal = false })
+    }
+}
+
+fun convertMillisToDate(millis: Long): String {
+    val formatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    val calendar = Calendar.getInstance()
+    calendar.timeInMillis = millis
+    calendar.add(Calendar.DAY_OF_MONTH, 1)
+    return formatter.format(calendar.time)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModal(
+    onDateSelected: (Long?) -> Unit, onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState()
+
+    DatePickerDialog(onDismissRequest = onDismiss, confirmButton = {
+        TextButton(onClick = {
+            onDateSelected(datePickerState.selectedDateMillis)
+            onDismiss()
+        }) {
+            Text(stringResource(R.string.ok))
+        }
+    }, dismissButton = {
+        TextButton(onClick = onDismiss) {
+            Text(stringResource(R.string.cancel))
+        }
+    }) {
+        DatePicker(state = datePickerState)
+    }
+}
